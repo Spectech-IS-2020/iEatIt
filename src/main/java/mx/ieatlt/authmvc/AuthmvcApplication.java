828346d44a4d062cf0fcf13edc5174727601270f
Raúl González <gonzalv@ciencias.unam.mx>
2020-05-01 19:11:33 -0500

    First commit

---
+++ src/main/java/mx/ieatlt/authmvc/AuthmvcApplication.java
@@ -0,0 +1,13 @@
+package mx.ieatlt.authmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthmvcApplication.class, args);
+	}
+
+}
