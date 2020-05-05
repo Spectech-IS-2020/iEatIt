b9d548675c1d1be2ac2f92726491858dd0db604c
jorgeruiz11 <jorgeruiz@ciencias.unam.mx>
2020-05-05 14:03:21 -0500

    Alimentos, añadido

---
+++ src/main/java/mx/spechtech/ieatlt/repositorio/RepositorioAlimento.java
@@ -0,0 +1,7 @@
+package mx.spechtech.ieatlt.repositorio;
+
+import mx.spechtech.ieatlt.modelo.Alimento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepositorioAlimento extends CrudRepository<Alimento, Integer> {
+}
