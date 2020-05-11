517e0cfa820470ea05d71dd09727d3b55aa0c64d
Raúl González <gonzalv@ciencias.unam.mx>
2020-05-11 13:46:43 -0500

    [ADDED] Mensaje de error al iniciar sesión

--- src/main/java/mx/spechtech/ieatit/IeatitApplication.java
+++ src/main/java/mx/spechtech/ieatit/IeatitApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IeatltApplication {
+public class IeatitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IeatltApplication.class, args);
+		SpringApplication.run(IeatitApplication.class, args);
 	}
 
 }
